347a26d216f0cb21cdbaf3a675ca3ed748b868d9
Anastazija04 <51087074+Anastazija04@users.noreply.github.com>
2020-07-14 13:30:20 +0200

    Add files via upload

---
+++ build/precache-manifest.5ce98969a889b3da52ed816d5d479abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc943451cfd090e594ded2869b46c64",
+    "url": "/periodictable/periodictable/index.html"
+  },
+  {
+    "revision": "591c4b787b1bca6c3ada",
+    "url": "/periodictable/periodictable/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "6b9312d32de160bcb3fd",
+    "url": "/periodictable/periodictable/static/css/main.7aa97660.chunk.css"
+  },
+  {
+    "revision": "591c4b787b1bca6c3ada",
+    "url": "/periodictable/periodictable/static/js/2.c4651211.chunk.js"
+  },
+  {
+    "revision": "6b9312d32de160bcb3fd",
+    "url": "/periodictable/periodictable/static/js/main.c49f1b99.chunk.js"
+  },
+  {
+    "revision": "cbc303b49f14657dc575",
+    "url": "/periodictable/periodictable/static/js/runtime~main.d900b1ae.js"
+  }
+]);
